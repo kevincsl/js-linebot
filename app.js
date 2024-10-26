fca36d5c47d28ee61a6729cd2c4c360fc8b51467
Chi Hsien Lin <kevincsl@gmail.com>
2024-10-26 18:31:43 +0800

    test v1

--- app.js
+++ app.js
@@ -4,35 +4,26 @@
 // 指令: node app.js
 
 // 設定 LINE Bot 的 Channel Access Token 和 Channel Secret
-// 設定 LINE Bot 的 Channel Access Token 和 Channel Secret
+// 指令: export LINE_CHANNEL_ACCESS_TOKEN
+// 指令: export LINE_CHANNEL_SECRET
+// 指令: export PORT=8000
+
 
 // 安裝 express and @line/bot-sdk 套件
 // 指令: npm install express @line/bot-sdk
-// body-parser 套件是用來解析 POST 請求的內容
-// 指令: npm install body-parser
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const line = require('@line/bot-sdk');
-
-const app = express();
-// 設定 port 預設參數 8000  
-// env 會用來判斷目前執行環境的 port
-// 如果有設定 port 環境變數，就使用設定的 port，否則使用 8000
-// 這樣可以讓程式在不同的環境上執行，不需要每次都手動設定 port
-// 指令: export PORT=8000
-const port = process.env.PORT || 8000;
+import express from 'express';
+import { middleware, Client } from '@line/bot-sdk';
 
 const config = {
-// 設定 LINE Bot 的 Channel Access Token 和 Channel Secret
-    channelAccessToken: 'u7CRiMZO93OhPzdxwO9D1qanyZ4ShycTHtM4lGTPAs9qAlQqnHSaQlFvNOgKy6rzu3vl5MPmOdsqKx1EecYGLR8AAZeKRPiHzNLerEDE1Ecod397T1ajfrkl+O4XB3SVStXWiTe9mStmWfU5q8m6MQdB04t89/1O/w1cDnyilFU=',
-    channelSecret: 'b44fedbc1b061b2ad22c9a9328adee25'
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
 };
 
+const client = new Client(config);
+const app = express();
 
-app.use(bodyParser.json());
-
-app.post('/', line.middleware(config), (req, res) => {
+app.post('/', middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result))
@@ -46,12 +37,12 @@ function handleEvent(event) {
     if (event.type !== 'message' || event.message.type !== 'text') {
         return Promise.resolve(null);
     }
-
+    const line_client_id = event.source.userId;
     const echo = { type: 'text', text: event.message.text };
-
-    return client.replyMessage(event.replyToken, echo);
+    return client.replyMessage(event.replyToken, echo+line_client_id);
 }
 
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
